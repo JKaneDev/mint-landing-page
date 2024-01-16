d07608de62a8092e649de1433f0399ff9d891b06
James Kane <jtkanedev@gmail.com>
2024-01-16 05:30:57 +0000

    project init + navbar setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     return (
+          <div className='bg-black'>
+               <nav className='flex items-center justify-between bg-gray-800 p-4'>
+                    <div className='text-white font-bold text-xl'>
+                         Site Name
+                    </div>
+                    <div className='flex space-x-4'>
+                         <button className='text-white'>Home</button>
+                         <button className='text-white'>About</button>
+                         <button className='text-white'>Mint</button>
+                         <button className='text-white'>Community</button>
+                    </div>
+                    <div>
+                         <button className='text-white'>Connect Wallet</button>
+                    </div>
+               </nav>
+          </div>
+     );
 }
 
 export default App;
